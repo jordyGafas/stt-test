71da0732dd136fad47e5fa517359883d28d481f9
jordyGafas <39955874+jordyGafas@users.noreply.github.com>
2023-12-04 10:57:45 +0100

    Fix top-level-wait

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  buid: {
+    target: 'esnext'
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
@@ -19,6 +23,12 @@ export default defineConfig({
         './App': './src/App.jsx'
       },
       shared: ['react']
+    }),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`
     })
   ]
 });
