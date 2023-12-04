17cb9a9663d5c1e982ac9dca4a1dcbf5ba71c402
jordyGafas <39955874+jordyGafas@users.noreply.github.com>
2023-12-04 10:53:49 +0100

    Add vite-plugin-federation

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,15 @@ export default defineConfig({
       babel: {
         plugins: ['@emotion/babel-plugin']
       }
+    }),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      // Modules to expose
+      exposes: {
+        './App': './src/App.jsx'
+      },
+      shared: ['react']
     })
   ]
 });
