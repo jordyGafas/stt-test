29751013244d29e8ba1ca32c74e954ba4a35a35e
jordyGafas <39955874+jordyGafas@users.noreply.github.com>
2023-12-04 13:16:56 +0100

    modal test

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
+import React, { useState, useEffect } from 'react';
 import logo from './assets/images/logo.svg';
+import { Modal } from './components/Modal';
 
 export const App = () => {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="app">
       <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
-        <p className="header">Vite React Starter 💯</p>
-        <p>
-          Vite + React <br />
-          ESLint + Prettier + Stylelint
-          <br />
-          Sass + Emotion + Tailwind
-          <br />
-          Jest + Testing Library
-        </p>
+        <p className="header">POC TEST</p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <button onClick={()=>setModalOpen(true)} id="openModal">Open the modal</button>
+        <Modal open={modalOpen} setModalOpen={()=>setModalOpen(false)} />
       </header>
     </div>
   );
---
+++ src/components/Modal.jsx
@@ -0,0 +1,15 @@
+export const Modal = ({open, setModalOpen}) => {
+  if (!open) return <></>;
+  return (
+    <div id="modal" class="modal">
+      <button onClick={()=>setModalOpen(false)} id="closeModal" class="modal-close-btn">
+        Close
+      </button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Nice modal</h1>
+    </div>
+  );
+};
